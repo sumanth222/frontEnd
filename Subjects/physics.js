8c3774afc9f940b5d9df8839205608a8150ca051
sumanth222 <70339782+sumanth222@users.noreply.github.com>
2020-09-17 17:48:26 +0530

    Add files via upload

--- Subjects/physics.js
+++ Subjects/physics.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View,Dimensions } from 'react-native';
+import { Platform, StyleSheet, Text, View,Dimensions,Image,Button } from 'react-native';
 import {Video} from 'expo-av';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 
 
@@ -25,8 +26,10 @@ export default class physicsScreen extends Component {
   render() {
       const {width,height} = Dimensions.get("window");
     return (
-      <SafeAreaView>
+        
+    
       <View style={styles.container}>
+         
        <Video
   source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
   rate={1.0}
@@ -38,11 +41,17 @@ export default class physicsScreen extends Component {
   isLooping
   style={{ width: "100%", height: height/2}}
 />
-<Text style={{fontSize: 30,marginTop:20,fontStyle:'bold'}}>Sample Video</Text>
+<Button
+  onPress={() => this.props.navigation.goBack(null)}
+  title="Back to courses"
+  color="#841584"
+  borderRadius= "20"
+  accessibilityLabel="Learn more about this purple button"
+/>
+<Text style={{fontSize: 30,marginTop:20}}>Sample Video</Text>
 <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
       </View>
 
-      </SafeAreaView>
     );
   }
 }
