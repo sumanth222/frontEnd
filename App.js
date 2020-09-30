bc3902432df2ed0db3d14cfa7946e6ca2216c7f8
sumanth222 <70339782+sumanth222@users.noreply.github.com>
2020-09-30 20:01:42 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -13,8 +13,8 @@ import SplashScreen from "./screens/splashScreen";
 import physicsScreen from './Subjects/physics';
 import physicsPlaylist from "./playlists/physicspl";
 import webcm from "./webcam/webcm";
-import App1 from "./App1";
-import App2 from "./App2";
+import App1 from "./addQuestion";
+import App2 from "./test";
 
 //import all the screens we are going to switch
 const App = createStackNavigator(
@@ -27,8 +27,8 @@ const App = createStackNavigator(
     physics : {screen : physicsScreen,navigationOptions: {header: null,} },
     physicsPlaylist : {screen : physicsPlaylist,navigationOptions: {header: null,} },
     teacher: { screen: teacherDash,navigationOptions: {header: null,}  },
-    App1 : {screen : App1,navigationOptions: {header: null,} },
-    App2: {screen : App2,navigationOptions: {header: null,} },
+    App1 : {screen : App1,navigationOptions:{title: "Go back"}},
+    App2: {screen : App2,navigationOptions:{title: "Go back"}},
     webcm:{screen : webcm}
     //First entry by default be our first screen if we do not define initialRouteName
   },  
